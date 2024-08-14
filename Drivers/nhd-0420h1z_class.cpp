77ca3900f933d8b82c05ef4ea3fabb7a2159ef4b
Alexander Webster <ajewebster@users.noreply.github.com>
2024-08-15 00:00:16 +0200

    Create nhd-0420h1z_class.cpp
    Start of a class version of the driver

---
+++ Drivers/nhd-0420h1z_class.cpp
@@ -0,0 +1,20 @@
+
+
+
+class LCDDriver
+{
+
+public:
+    /** Create an LCDDriver for use with the shift register
+    *
+    *    @param chip_select DigitalOut object used as CS line
+    *    @param serial_port SPI object to communicate with the shitf register
+    */
+    LCDDriver(DigitalOut* chip_select, SPI* serial_port)
+        : m_CS {chip_select}
+        , m_serial_port {serial_port}
+
+private:
+    DigitalOut* m_CS;
+    SPI* m_serial_port;
+}
